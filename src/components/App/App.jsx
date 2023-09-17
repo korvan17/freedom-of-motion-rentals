1203481b8e84c81aabcfdbb763769fbdddeee0d6
korvan17 <ivan@homeds.pro>
2023-09-17 20:13:15 +0300

    fix load catalog

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,14 +3,16 @@ import Layout from '../Layout/Laypout';
 import CatalogPage from '../../pages/CatalogPage';
 import FavoritesPage from '../../pages/FavoritesPage';
 import HomePage from '../../pages/HomePage';
+import { useGetCarsQuery } from '../../redux/carSlice';
 
 export default function App() {
+  const { isSuccess } = useGetCarsQuery();
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<HomePage />} />
-        <Route path="catalog" element={<CatalogPage />} />
-        <Route path="favorites" element={<FavoritesPage />} />
+        {isSuccess && <Route path="catalog" element={<CatalogPage />} />}
+        {isSuccess && <Route path="favorites" element={<FavoritesPage />} />}
         <Route path="*" element={<Navigate to="/" />} />
       </Route>
     </Routes>
