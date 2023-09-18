60a6a335f9e1f5e0906192a04dd0a3e055982b14
korvan17 <ivan@homeds.pro>
2023-09-18 08:40:39 +0300

    add persistor

--- src/index.js
+++ src/index.js
@@ -3,18 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { App } from './components';
 import { Provider } from 'react-redux';
-// import { PersistGate } from "redux-persist/integration/react";
-// import { persistor, store } from './redux/store';
-import { store } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor, store } from './redux/store';
 import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    {/* <PersistGate loading={null} persistor={persistor}> */}
-    <BrowserRouter basename="/freedom-of-motion-rentals">
-      <App />
-    </BrowserRouter>
-    {/* </PersistGate> */}
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter basename="/freedom-of-motion-rentals">
+        <App />
+      </BrowserRouter>
+    </PersistGate>
   </Provider>
 );
